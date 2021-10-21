2819b06ce048d3edb4ab2ff7a4a5981c71362458
abeidahmed <abeidahmed92@gmail.com>
2021-10-21 12:21:04 +0400

    dynamically state label dismiss button

--- docs/stories/Label.tsx
+++ docs/stories/Label.tsx
@@ -13,9 +13,11 @@ export const Label = ({ text, scheme, circle, dismissable, ...props }: LabelProp
     <span className={joinClasses(['label', circle && 'label--circle', `label--${scheme}`])} {...props}>
       {text}
 
-      <button type="button" className="label-close">
-        <Icon icon="x" size={18} />
-      </button>
+      {dismissable && (
+        <button type="button" className="label-close">
+          <Icon icon="x" size={18} />
+        </button>
+      )}
     </span>
   )
 }
